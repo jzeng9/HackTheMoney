6691112098d75e8ab9f65370c193ac9c22f43b74
strange <519134691@qq.com>
2017-05-08 20:35:24 +0800

    Initial commit

---
+++ app/src/main/java/com/smie/hackthemoney/MainActivity.java
@@ -0,0 +1,13 @@
+package com.smie.hackthemoney;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
